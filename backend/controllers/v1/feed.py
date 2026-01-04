fc8148968a415486f7c760862fb1fefdf4df62da
Rohit Vishwakarma <wishrohitv@proton.me>
2026-01-04 14:36:04 +0530

    Improvement in feed query

--- backend/controllers/v1/feed.py
+++ backend/controllers/v1/feed.py
@@ -1,21 +1,27 @@
-from backend.modules import Blueprint, make_response
-from backend.repository.feedRepository import getHomeFeed, getUsersPostsFeed
+from backend.config import API_ENDPOINTS
+from backend.middlewares.verifyClientRequest import verifyRequestMiddleware
+from backend.modules import Blueprint, make_response, request
+from backend.repository.feedRepository import getHomeFeed
+from backend.utils import LoggedUser
 
 feedBlueprint = Blueprint("feed", __name__)
 
+route = API_ENDPOINTS()
 
-@feedBlueprint.route("/feed", methods=["GET"])
-def getFeed():
+
+# /feed
+@feedBlueprint.route(route.feed.routeName, methods=route.feed.methods)
+@verifyRequestMiddleware(route.feed.routeName)
+def getFeed(loggedUser: LoggedUser | None = None, *args, **kwargs):
     """
     Check if user is logged then build home feed based on his interests
     """
-    return getHomeFeed()
-
-
-@feedBlueprint.route("feed/user/<int:userID>", methods=["GET"])
-def getUserPost(userID):
-    """
-    if user is logged then return private and public posts else public
-    Args: .
-    """
-    return getUsersPostsFeed(userID=userID)
+    offset = request.args.get("offset", type=int, default=0)
+    categoryIDs = request.args.get("category", type=list, default=[1])
+    sessionUserID: int | None = loggedUser.userID if loggedUser else None
+    try:
+        return getHomeFeed(
+            category=categoryIDs, offset=offset, sessionUserID=sessionUserID
+        )
+    except Exception as e:
+        return make_response({"error": str(e)}, 500)
--- backend/repository/feedRepository.py
+++ backend/repository/feedRepository.py
@@ -1,8 +1,11 @@
 from backend.database import engine
-from backend.models import Category, Posts, Users
+from backend.models import Bookmark, Category, Likes, Posts, Users
 from backend.modules import (
     API_ROOT_URL,
     USE_CLOUDINARY_STORAGE,
+    aliased,
+    exists,
+    func,
     json,
     make_response,
     request,
@@ -15,27 +18,37 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
-def getHomeFeed(category: str = "all", offset: int = 0):
+def getHomeFeed(
+    category: list = [],
+    offset: int = 0,
+    sessionUserID: int | None = None,
+):
     # Get feed data from database alog with userName of author of post
+    likeCount = aliased(Likes)
+    bookmarkCount = aliased(Bookmark)
     getFeedData = (
         select(
             Users.userName,
             Posts,
-            # Posts.id,
-            # Posts.title,
-            # Posts.tags,
-            # Posts.mediaUrl,
-            # Posts.mediaPublicID,
-            # Posts.fileType,
-            # Posts.fileExtension,
-            # Posts.visibility,
-            # Posts.ageRating,
-            # Posts.category,
+            func.count(likeCount.userID).label("likeCount"),
+            func.count(bookmarkCount.userID).label("bookmarkCount"),
+            exists(
+                select(Likes).where(
+                    Likes.postID == Posts.id, Likes.userID == sessionUserID
+                )
+            ).label("isLiked"),
+            exists(
+                select(Bookmark).where(
+                    Bookmark.postID == Posts.id, Bookmark.userID == sessionUserID
+                )
+            ).label("isBookmarked"),
         )
         .join_from(Users, Posts)
-        .where(Posts.visibility == True)
-    )  # .where(Posts.userID == Users.id)
-    # print(getFeedData)
+        .where(Posts.visibility)  # = True
+        .outerjoin(likeCount, likeCount.postID == Posts.id)
+        .outerjoin(bookmarkCount, bookmarkCount.postID == Posts.id)
+        .group_by(Posts.id, Users.userName)
+    )
     getFeed = session.execute(getFeedData).all()
 
     # Close the session
@@ -62,60 +75,10 @@ def getHomeFeed(category: str = "all", offset: int = 0):
                     "postMediaUrl": feed[1].mediaUrl
                     if USE_CLOUDINARY_STORAGE
                     else f"{API_ROOT_URL}{url_for('postMedia.servePostMedia', fileName=f'{feed[1].mediaPublicID}.{feed[1].fileExtension}')}",
-                }
-                feedObj.append(data)
-            return make_response({"payload": feedObj}, 200)
-        else:
-            return make_response({"payload": []}, 200)
-
-    except Exception as e:
-        return make_response({"error": f"{e}"}, 401)
-
-
-def getUsersPostsFeed(category: str = "all", offset: int = 0, userID: int = None):
-    # Get feed data from database alog with userName of author of post
-    getFeedData = (
-        select(
-            Users.userName,
-            Posts,
-            # Posts.id,
-            # Posts.title,
-            # Posts.tags,
-            # Posts.mediaUrl,
-            # Posts.fileType,
-            # Posts.fileExtension,
-            # Posts.visibility,
-            # Posts.ageRating,
-            # Posts.category,
-        )
-        .join_from(Users, Posts)
-        .where(Posts.userID == userID)
-    )
-    # print(getFeedData)
-    getFeed = session.execute(getFeedData).all()
-    # print(getFeed)
-    # Close the session
-    session.close()
-    try:
-        if getFeed:
-            feedObj = []
-            for feed in getFeed:
-                data = {
-                    "userName": feed[0],
-                    "postID": feed[1].id,
-                    "userID": feed[1].userID,
-                    "title": feed[1].title,
-                    "tags": feed[1].tags,
-                    "mediaPublicID": feed[1].mediaPublicID,
-                    "fileType": feed[1].fileType,
-                    "fileExtension": feed[1].fileExtension,
-                    "visibility": feed[1].visibility,
-                    "ageRating": feed[1].ageRating.value,
-                    "category": feed[1].category,
-                    "postUserPicUrl": f"{API_ROOT_URL}{url_for('profileImage.serveImage', fileName=feed[0])}",
-                    "postMediaUrl": feed[1].mediaUrl
-                    if USE_CLOUDINARY_STORAGE
-                    else f"{API_ROOT_URL}{url_for('postMedia.servePostMedia', fileName=f'{feed[1].postUrl}.{feed[1].fileExtension}')}",
+                    "likeCount": feed[2],
+                    "bookmarkCount": feed[3],
+                    "isLiked": feed[4],
+                    "isBookmarked": feed[5],
                 }
                 feedObj.append(data)
             return make_response({"payload": feedObj}, 200)
