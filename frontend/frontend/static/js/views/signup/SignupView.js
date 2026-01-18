1fb638939bc4bb72af83fa3096b9b022b155a925
Rohit Vishwakarma <wishrohitv@proton.me>
2026-01-18 21:48:17 +0530

    Optimize code and fix bug

--- frontend/frontend/static/js/views/signup/SignupView.js
+++ frontend/frontend/static/js/views/signup/SignupView.js
@@ -1,4 +1,5 @@
 import AbstractView from "../AbstractView.js";
+import { apiSignupUser } from "../../utils/base.js";
 
 export default class extends AbstractView {
   constructor(params) {
@@ -12,13 +13,43 @@ export default class extends AbstractView {
         "/static/js/views/signup/templates/daisyUI/signup.html",
       );
       const page = await html.text();
-      this.page = page;
+      this.page = document.createElement("div");
+      this.page.innerHTML = page;
+
+      this.form = this.page.querySelector("#signupForm");
+
+      this.form.addEventListener("submit", async (e) => {
+        e.preventDefault(); // Stop the page from reloading
+
+        const formData = new FormData(this.form);
+        const data = Object.fromEntries(formData.entries()); // Convert form to simple object
+        console.log(data);
+        await this.signup(data);
+      });
     } catch (error) {
-      console.error(error);
-      this.page = "Error loading signup page";
+      console.error("Error fetching HTML:", error);
+      this.page = "<h1>Error</h1><p>Failed to signup page</p>";
     }
-    const html = document.createElement("div");
-    html.innerHTML = this.page;
-    return html;
+    return this.page;
+  }
+
+  async signup(data) {
+    try {
+      const connection = await fetch(apiSignupUser, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const response = await connection.json();
+      if (connection.status === 201) {
+        window.location.href = "/auth/login";
+      } else {
+        console.error("Signup failed", response);
+      }
+    } catch (e) {
+      console.error("Signup failed : ", e);
+    }
   }
 }
