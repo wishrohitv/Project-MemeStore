0bbf3dc51f2535b906330cf1e90b1deeb89d06a5
Rohit Vishwakarma <wishrohitv@proton.me>
2026-01-18 14:38:51 +0530

    Optimize route handling

--- frontend/frontend/app.py
+++ frontend/frontend/app.py
@@ -19,23 +19,14 @@ def createApp():
     from frontend.utils.contextProcessors.constants import constants
     from frontend.utils.contextProcessors.dateTime import dateTime
 
-    ## Pages
-
     ## Register context processors
     app.context_processor(dateTime)
     app.context_processor(constants)
 
     # Register Index page
-    @app.route("/*")
-    def index():
+    @app.route("/", defaults={"path": ""})
+    @app.route("/<path:path>")
+    def index(path):
         return render_template("base.html")
 
-    # Error handler
-    @app.errorhandler(404)
-    # @app.errorhandler(werkzeug.exceptions.BadRequest)
-    def handle_bad_request(e):
-        return render_template("badRequest.html"), 404
-
-    # app.url_map.strict_slashes=True
-    # print(app.url_map)
     return app
