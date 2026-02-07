10c782c05ed8b5cc67bb43ba7f3633694b254794
Rohit Vishwakarma <wishrohitv@proton.me>
2026-02-07 20:36:10 +0530

    Remove old redundant code with new post card thats help control post card js and html logic from one place

--- frontend/app/static/js/views/posts/PostView.js
+++ frontend/app/static/js/views/posts/PostView.js
@@ -7,6 +7,7 @@ import {
   apiTogglePostBookmark,
 } from "../../utils/base.js";
 import { formatDate } from "../../utils/datetime.js";
+import { postCard } from "../../utils/postCard.js";
 
 export default class extends AbstractView {
   constructor(params) {
@@ -24,7 +25,9 @@ export default class extends AbstractView {
       const page = await html.text();
       this.page = document.createElement("div");
       this.page.innerHTML = page;
+      this.postContainer = this.page.querySelector(".postCard");
       this.repliesContainer = this.page.querySelector("#repliesContainer");
+      this.spinner = this.page.querySelector("#spinner");
       this.form = this.page.querySelector("#commentForm");
       this.form.addEventListener("submit", async (e) => {
         e.preventDefault();
@@ -42,203 +45,27 @@ export default class extends AbstractView {
 
   async fetchPost() {
     try {
-      const connection = await fetch(`${apiUserPostsFeed}/${this.postID}`, {
-        credentials: "include",
+      this.spinner.classList.remove("hidden");
+      initializeTemplate({}).then(async (postTemplate) => {
+        /// Fetch home feed data
+        const connection = await fetch(`${apiUserPostsFeed}/${this.postID}`, {
+          credentials: "include",
+        });
+
+        const response = await connection.json();
+        const post = response.payload[0];
+        if (connection.ok) {
+          const clone = postTemplate.content.cloneNode(true);
+          const card = await postCard(clone, post, { type: "post" });
+          this.postContainer.appendChild(card);
+        }
+        if (connection.status !== 404) {
+          if (post.replieCount !== 0) {
+            this.fetchPostReplies();
+          }
+        }
+        this.spinner.classList.add("hidden");
       });
-
-      const response = await connection.json();
-      const post = response.payload[0];
-      if (connection.ok) {
-        const clone = this.page;
-        clone.querySelector(".qoute").addEventListener("click", (e) => {
-          this.navigator("/post/create");
-        });
-        this.setTitle(post.title ?? post.userName);
-        clone.querySelector(".cardTitle").textContent = post.title;
-        clone.querySelector(".cardInfo").textContent = post.tags;
-        clone.querySelector(".postUserName").textContent = post.userName;
-
-        if (post.fileType) {
-          clone.querySelector(".media").classList.remove("hidden");
-          if (post.fileType === "image") {
-            clone.querySelector(".postContentImgPreview").src =
-              post.postMediaUrl;
-          } else if (post.fileType === "video") {
-            clone
-              .querySelector(".postContentImgPreview")
-              .classList.add("hidden");
-            clone
-              .querySelector(".postContentVidPreview")
-              .classList.remove("hidden");
-            clone.querySelector(".postContentVidPreview").src =
-              post.postMediaUrl;
-          }
-          clone.querySelector(".postUserPic").src = post.postUserPicUrl;
-        }
-        // Post like
-        const likeBtn = clone.querySelector(".likeBtn");
-        if (post.likeCount !== 0) {
-          likeBtn.querySelector(".count").innerText = post.likeCount;
-        }
-
-        if (post.isLiked) {
-          likeBtn.querySelector(".svgs").children[0].classList.add("hidden");
-          likeBtn.querySelector(".svgs").children[1].classList.remove("hidden");
-        }
-        likeBtn
-          .querySelector(".svgs")
-          .addEventListener("click", async (event) => {
-            try {
-              let conn = await fetch(`${apiTogglePostLike}/${post.postID}`, {
-                method: "PUT",
-                credentials: "include",
-              });
-              let res = await conn.json();
-              if (conn.ok) {
-                if (res.isLiked) {
-                  event.target.parentNode.parentNode.children[1].classList.remove(
-                    "hidden",
-                  );
-                  event.target.parentNode.parentNode.children[0].classList.add(
-                    "hidden",
-                  );
-                  likeBtn.querySelector(".count").innerText =
-                    post.likeCount + 1;
-                } else {
-                  event.target.parentNode.parentNode.children[1].classList.add(
-                    "hidden",
-                  );
-                  event.target.parentNode.parentNode.children[0].classList.remove(
-                    "hidden",
-                  );
-                  likeBtn.querySelector(".count").innerText =
-                    post.likeCount - 1;
-                }
-              }
-              console.log(res);
-            } catch (e) {
-              console.error(e);
-            }
-          });
-        // Post Bookmark
-        const bookmarkBtn = clone.querySelector(".bookmarkBtn");
-        if (post.bookmarkCount !== 0) {
-          bookmarkBtn.querySelector(".count").innerText = post.bookmarkCount;
-        }
-
-        if (post.isBookmarked) {
-          bookmarkBtn
-            .querySelector(".svgs")
-            .children[0].classList.add("hidden");
-          bookmarkBtn
-            .querySelector(".svgs")
-            .children[1].classList.remove("hidden");
-        }
-        bookmarkBtn
-          .querySelector(".svgs")
-          .addEventListener("click", async (event) => {
-            try {
-              let conn = await fetch(
-                `${apiTogglePostBookmark}/${post.postID}`,
-                {
-                  method: "PUT",
-                  credentials: "include",
-                },
-              );
-              let res = await conn.json();
-              if (conn.ok) {
-                if (res.isBookmarked) {
-                  event.target.parentNode.parentNode.children[1].classList.remove(
-                    "hidden",
-                  );
-                  event.target.parentNode.parentNode.children[0].classList.add(
-                    "hidden",
-                  );
-                  bookmarkBtn.querySelector(".count").innerText =
-                    post.bookmarkCount + 1;
-                } else {
-                  event.target.parentNode.parentNode.children[1].classList.add(
-                    "hidden",
-                  );
-                  event.target.parentNode.parentNode.children[0].classList.remove(
-                    "hidden",
-                  );
-                  bookmarkBtn.querySelector(".count").innerText =
-                    post.bookmarkCount - 1;
-                }
-              }
-              console.log(res);
-            } catch (e) {
-              console.error(e);
-            }
-          });
-        // Download Button
-        const downloadBtn = clone.querySelector(".downloadBtn");
-        downloadBtn.addEventListener("click", (e) => {
-          window.location.href = `${apiGetPostMedia}/${post.postID}`;
-          // Good for showing ads
-          // window.open(
-          //   `${apiGetPostMedia}/${post.postID}`,
-          //   "_blank",
-          // );
-        });
-        // Link of creator's profile
-        clone.querySelector(".postUserPic").src = post.profileImgUrl;
-        clone.querySelector(".postUserName").href = `/user/${post.userName}`;
-        clone.querySelector(".userProfileLink").href = `/user/${post.userName}`;
-        clone.querySelector(".createdAt").innerText = formatDate(
-          post.createdAt,
-        );
-
-        // Load parent post
-        if (post.parentPostID) {
-          const connection = await fetch(
-            `${apiUserPostsFeed}/${post.parentPostID}`,
-            {
-              credentials: "include",
-            },
-          );
-          let parentPostObj = await connection.json();
-
-          if (connection.ok) {
-            const parentPost = parentPostObj.payload[0];
-            let parentPostContainer = clone.querySelector(
-              ".parentPostContainer",
-            );
-            if (parentPostContainer) {
-              parentPostContainer.classList.remove("hidden");
-              const parentMacro = await initializeTemplate({
-                macro: "parent",
-              });
-              const cloneParentMacro = parentMacro.content.cloneNode(true);
-              cloneParentMacro
-                .querySelector(".card")
-                .addEventListener("click", (e) => {
-                  if (e.target.closest(".card")) {
-                    this.navigator("/post/" + parentPost.postID);
-                  }
-                });
-              cloneParentMacro.querySelector(".cardTitle").textContent =
-                parentPost.title;
-              cloneParentMacro.querySelector(".postUserName").textContent =
-                parentPost.userName;
-              cloneParentMacro.querySelector(".userProfileLink").href =
-                `/user/${parentPost.userName}`;
-              cloneParentMacro.querySelector(".postUserPic").src =
-                parentPost.profileImgUrl;
-              cloneParentMacro.querySelector(".createdAt").innerText =
-                formatDate(parentPost.createdAt);
-
-              parentPostContainer.appendChild(cloneParentMacro);
-            }
-          }
-        }
-      }
-      if (connection.status !== 404) {
-        if (post.replieCount !== 0) {
-          this.fetchPostReplies();
-        }
-      }
     } catch (e) {
       console.error(e);
     }
@@ -256,202 +83,9 @@ export default class extends AbstractView {
         // Loop posts list
         posts.payload.forEach(async (post) => {
           const clone = postTemplate.content.cloneNode(true);
-          clone.querySelector(".card").addEventListener("click", (e) => {
-            if (
-              e.target.closest(
-                "a, button, .likeBtn, .bookmarkBton, .shareBtn, .downloadBtn, #moreBtnContainer",
-              )
-            )
-              return;
-            this.navigator("/post/" + post.postID);
-          });
-          clone.querySelector(".qoute").addEventListener("click", (e) => {
-            this.navigator("/post/create");
-          });
-          clone.querySelector(".cardTitle").textContent = post.title;
-          clone.querySelector(".cardInfo").textContent = post.tags;
-          clone.querySelector(".postUserName").textContent = post.userName;
+          let card = await postCard(clone, post, {});
 
-          if (post.fileType) {
-            clone.querySelector(".media").classList.remove("hidden");
-            if (post.fileType === "image") {
-              clone.querySelector(".postContentImgPreview").src =
-                post.postMediaUrl;
-            } else if (post.fileType === "video") {
-              clone
-                .querySelector(".postContentImgPreview")
-                .classList.add("hidden");
-              clone
-                .querySelector(".postContentVidPreview")
-                .classList.remove("hidden");
-              clone.querySelector(".postContentVidPreview").src =
-                post.postMediaUrl;
-            }
-            clone.querySelector(".postUserPic").src = post.postUserPicUrl;
-          }
-          // Post like
-          const likeBtn = clone.querySelector(".likeBtn");
-          if (post.likeCount !== 0) {
-            likeBtn.querySelector(".count").innerText = post.likeCount;
-          }
-
-          if (post.isLiked) {
-            likeBtn.querySelector(".svgs").children[0].classList.add("hidden");
-            likeBtn
-              .querySelector(".svgs")
-              .children[1].classList.remove("hidden");
-          }
-          likeBtn
-            .querySelector(".svgs")
-            .addEventListener("click", async (event) => {
-              try {
-                let conn = await fetch(`${apiTogglePostLike}/${post.postID}`, {
-                  method: "PUT",
-                  credentials: "include",
-                });
-                let res = await conn.json();
-                if (conn.ok) {
-                  if (res.isLiked) {
-                    event.target.parentNode.parentNode.children[1].classList.remove(
-                      "hidden",
-                    );
-                    event.target.parentNode.parentNode.children[0].classList.add(
-                      "hidden",
-                    );
-                    likeBtn.querySelector(".count").innerText =
-                      post.likeCount + 1;
-                  } else {
-                    event.target.parentNode.parentNode.children[1].classList.add(
-                      "hidden",
-                    );
-                    event.target.parentNode.parentNode.children[0].classList.remove(
-                      "hidden",
-                    );
-                    likeBtn.querySelector(".count").innerText =
-                      post.likeCount - 1;
-                  }
-                }
-                console.log(res);
-              } catch (e) {
-                console.error(e);
-              }
-            });
-          // Post Bookmark
-          const bookmarkBtn = clone.querySelector(".bookmarkBtn");
-          if (post.bookmarkCount !== 0) {
-            bookmarkBtn.querySelector(".count").innerText = post.bookmarkCount;
-          }
-
-          if (post.isBookmarked) {
-            bookmarkBtn
-              .querySelector(".svgs")
-              .children[0].classList.add("hidden");
-            bookmarkBtn
-              .querySelector(".svgs")
-              .children[1].classList.remove("hidden");
-          }
-          bookmarkBtn
-            .querySelector(".svgs")
-            .addEventListener("click", async (event) => {
-              try {
-                let conn = await fetch(
-                  `${apiTogglePostBookmark}/${post.postID}`,
-                  {
-                    method: "PUT",
-                    credentials: "include",
-                  },
-                );
-                let res = await conn.json();
-                if (conn.ok) {
-                  if (res.isBookmarked) {
-                    event.target.parentNode.parentNode.children[1].classList.remove(
-                      "hidden",
-                    );
-                    event.target.parentNode.parentNode.children[0].classList.add(
-                      "hidden",
-                    );
-                    bookmarkBtn.querySelector(".count").innerText =
-                      post.bookmarkCount + 1;
-                  } else {
-                    event.target.parentNode.parentNode.children[1].classList.add(
-                      "hidden",
-                    );
-                    event.target.parentNode.parentNode.children[0].classList.remove(
-                      "hidden",
-                    );
-                    bookmarkBtn.querySelector(".count").innerText =
-                      post.bookmarkCount - 1;
-                  }
-                }
-                console.log(res);
-              } catch (e) {
-                console.error(e);
-              }
-            });
-          // Download Button
-          const downloadBtn = clone.querySelector(".downloadBtn");
-          downloadBtn.addEventListener("click", (e) => {
-            window.location.href = `${apiGetPostMedia}/${post.postID}`;
-            // Good for showing ads
-            // window.open(
-            //   `${apiGetPostMedia}/${post.postID}`,
-            //   "_blank",
-            // );
-          });
-          // Link of creator's profile
-          clone.querySelector(".postUserPic").src = post.profileImgUrl;
-          clone.querySelector(".postUserName").href = `/user/${post.userName}`;
-          clone.querySelector(".userProfileLink").href =
-            `/user/${post.userName}`;
-          clone.querySelector(".createdAt").innerText = formatDate(
-            post.createdAt,
-          );
-
-          // Load parent post
-          if (post.parentPostID) {
-            const connection = await fetch(
-              `${apiUserPostsFeed}/${post.parentPostID}`,
-              {
-                credentials: "include",
-              },
-            );
-            let parentPostObj = await connection.json();
-
-            if (connection.ok) {
-              const parentPost = parentPostObj.payload[0];
-              let parentPostContainer = clone.querySelector(
-                ".parentPostContainer",
-              );
-              if (parentPostContainer) {
-                parentPostContainer.classList.remove("hidden");
-                const parentMacro = await initializeTemplate({
-                  macro: "parent",
-                });
-                const cloneParentMacro = parentMacro.content.cloneNode(true);
-                cloneParentMacro
-                  .querySelector(".card")
-                  .addEventListener("click", (e) => {
-                    if (e.target.closest(".card")) {
-                      this.navigator("/post/" + parentPost.postID);
-                    }
-                  });
-                cloneParentMacro.querySelector(".cardTitle").textContent =
-                  parentPost.title;
-                cloneParentMacro.querySelector(".postUserName").textContent =
-                  parentPost.userName;
-                cloneParentMacro.querySelector(".userProfileLink").href =
-                  `/user/${parentPost.userName}`;
-                cloneParentMacro.querySelector(".postUserPic").src =
-                  parentPost.profileImgUrl;
-                cloneParentMacro.querySelector(".createdAt").innerText =
-                  formatDate(parentPost.createdAt);
-
-                parentPostContainer.appendChild(cloneParentMacro);
-              }
-            }
-          }
-
-          this.repliesContainer.appendChild(clone);
+          this.repliesContainer.appendChild(card);
         });
       });
     }
