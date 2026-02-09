6f0c4269dbdd4ae595ff241d3dc43d7a9cd13f1c
Rohit Vishwakarma <wishrohitv@proton.me>
2026-02-09 22:25:25 +0530

    Add replie preview to replie box

--- frontend/app/static/js/utils/replieCard.js
+++ frontend/app/static/js/utils/replieCard.js
@@ -1,4 +1,5 @@
 import { apiUploadPosts } from "./base.js";
+import { createMediaPreview } from "./replieMediaPreview.js";
 
 export async function replieCard(postID) {
   try {
@@ -7,35 +8,45 @@ export async function replieCard(postID) {
     const replieMarco = document.createElement("div");
     replieMarco.innerHTML = replieHtml;
     const form = replieMarco.querySelector("form");
+    const imgPreview = replieMarco.querySelector("#imgPreview");
+    const selectedFile = replieMarco.querySelector("#selectedFile");
     form.addEventListener("submit", async (e) => {
       e.preventDefault();
       const formData = new FormData(form);
-
-      const connection = await fetch(
-        apiUploadPosts + `?parentPostID=${postID}&isReplie=True`,
-        {
-          method: "POST",
-          credentials: "include",
-          body: formData,
-        },
-      );
-      console.log(connection.status);
-      console.log(await connection.json());
+      console.log(formData);
+      try {
+        const connection = await fetch(
+          apiUploadPosts + `?parentPostID=${postID}&isReplie=True`,
+          {
+            method: "POST",
+            credentials: "include",
+            body: formData,
+          },
+        );
+        console.log(connection.status);
+        console.log(await connection.json());
+      } catch (e) {
+        console.error(e);
+      }
     });
-    const imgPreview = replieMarco.querySelector("#imgPreview");
+
+    // Listen button click event and fire file picker
     replieMarco
       .querySelector("#filePickerBtn")
       .addEventListener("click", (e) => {
-        const filePicker = showOpenFilePicker();
-        console.log(filePicker);
-        filePicker
-          .then((file) => {
-            console.log(file);
-            imgPreview.src = file.name;
-            imgPreview.classList.remove("hidden");
-          })
-          .catch((error) => console.log(error));
+        selectedFile.click();
       });
+    // Listen file change event and preview image
+    selectedFile.addEventListener("change", async (e) => {
+      const file = e.target.files[0];
+      if (file) {
+        imgPreview.classList.remove("hidden");
+        if (imgPreview.children.length > 0) {
+          imgPreview.removeChild(imgPreview.firstChild);
+        }
+        imgPreview.appendChild(await createMediaPreview(file));
+      }
+    });
     return replieMarco;
   } catch (error) {
     console.error(error);
