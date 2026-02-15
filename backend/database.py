6b5035cea7556e63b157b4939b42c9151db01aba
Rohit Vishwakarma <wishrohitv@proton.me>
2026-02-15 20:27:47 +0530

    Add redis db initialization

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,17 @@
+import redis
 from dotenv import load_dotenv
 
 from backend.modules import create_engine, os
 
 load_dotenv()
 
+# Initialize database engine
 engine = create_engine(os.environ.get("DB_URL") or "", pool_pre_ping=True)
+
+# Initialize Redis client
+redisClient = redis.Redis.from_url(
+    os.environ.get("REDIS_URL") or "", decode_responses=True
+)
 
 
 def initializeDb():
