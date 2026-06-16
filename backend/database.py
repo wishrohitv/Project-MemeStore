38ff4ba6fdacbe7231dbe481773f0b9db5e6426f
Rohit Vishwakarma <wishrohitv@proton.me>
2026-06-16 12:15:48 +0530

    Improve code and add error handling

--- backend/database.py
+++ backend/database.py
@@ -1,22 +1,32 @@
+import sys
+
 import redis
 from dotenv import load_dotenv
 from modules import create_engine, os, sessionmaker
+from redis import RedisError
 
 load_dotenv()
 
-# Initialize database engine
-engine = create_engine(os.environ.get("DB_URL") or "", pool_pre_ping=True)
+try:
+    # Initialize database engine
+    engine = create_engine(os.environ.get("DB_URL") or "", pool_pre_ping=True)
 
-SessionLocal = sessionmaker(
-    bind=engine,
-    autoflush=False,
-    autocommit=False,
-)
+    SessionLocal = sessionmaker(
+        bind=engine,
+        autoflush=False,
+        autocommit=False,
+    )
 
-# Initialize Redis client
-redis_client = redis.Redis.from_url(
-    os.environ.get("REDIS_URL") or "", decode_responses=True
-)
+    # Initialize Redis client
+    redis_client = redis.Redis.from_url(
+        os.environ.get("REDIS_URL") or "", decode_responses=True
+    )
+except RedisError as e:
+    print(f"Failed to initialize Redis client: {e}")
+    sys.exit(1)
+except Exception as e:
+    print(f"Failed to initialize database client: {e}")
+    sys.exit(1)
 
 
 def initialize_db():
